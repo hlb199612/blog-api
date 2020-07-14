bca68751236ddbe2afab806d603ebb6f4e8e6d32
haolinbo <952383184@qq.com>
2020-07-14 18:11:53 +0800

    提交数据库文件更新

--- config.js
+++ config.js
@@ -2,19 +2,19 @@ const config = {                              // 默认的dev配置
   DEBUG: true,                                    // 是否调试模式
   // MySQL数据库连接配置
   MYSQL: {
-    host: '121.196.126.70',
+    host: 'localhost',
     database: 'blog',
     username: 'root',
-    password: 'hlb199612'
+    password: 'root'
   }
 };
 if (process.env.NODE_ENV === 'production') {
   // 生产环境的MySQL数据库连接配置
   config.MYSQL = {
-    host: 'aaa.mysql.rds.aliyuncs.com',
-    database: 'aaa',
-    username: 'aaa',
-    password: 'aaa'
+    host: '121.196.126.70',
+      database: 'blog',
+      username: 'root',
+      password: 'hlb199612'
   };
 }
 module.exports = config;                  // 导出配置
